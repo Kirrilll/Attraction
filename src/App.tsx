59f48c68d647b2bac32e828067070b54a480e04f
DESKTOP-FRTFPVS\User <k.larionov.02@mail.ru>
2022-02-23 09:40:36 +0300

    add light

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,6 @@ function App() {
   return (
     <>
       <Canvas>
-        <spotLight intensity={1} position={[1, 0, 10]}></spotLight>
         <SceneManager></SceneManager>
       </Canvas>
     </>
--- src/components/attraction-scene/attractionScene.tsx
+++ src/components/attraction-scene/attractionScene.tsx
@@ -1,6 +1,6 @@
 import React, { ReactElement, ReactNode } from 'react';
 import * as THREE from 'three'
-import { Edges, Html, OrbitControls } from '@react-three/drei';
+import { BakeShadows, Edges, Html, OrbitControls, softShadows } from '@react-three/drei';
 import { ATTRACTION_SIZE } from '../../constans';
 
 
@@ -9,12 +9,31 @@ const AttractionScene: React.FC<{ child: ReactNode }> = (props) => {
 
     return (
         <group>
-            <OrbitControls></OrbitControls>
+            
+            <OrbitControls
+                maxDistance = {10} //сделать это множителем на размер здания
+                minDistance = {2} 
+                enablePan = {false}
+            ></OrbitControls>
             <Html>
                 <div>Какой-то контент</div>
             </Html>
-            <mesh scale = {ATTRACTION_SIZE}>
+
+            {/* Fill */}
+            <ambientLight intensity={0.5} />
+            {/* Main */}
+            <directionalLight
+                position={[1, 10, -2]}
+                intensity={1}
+            />
+            {/* Strip */}
+            <directionalLight position={[-10, -10, 2]} intensity={3} />
+            <mesh scale={ATTRACTION_SIZE} position={[0, -1, 0]}>
                 {props.child}
+            </mesh>
+            <mesh rotation = {[-Math.PI / 2, 0, 0]} position = {[0, -1 + ATTRACTION_SIZE, 0]} receiveShadow>
+                <planeBufferGeometry attach = 'geometry' args = {[100, 100]}></planeBufferGeometry>
+                <meshBasicMaterial color = 'hotpink'></meshBasicMaterial> 
             </mesh>
         </group>
 
